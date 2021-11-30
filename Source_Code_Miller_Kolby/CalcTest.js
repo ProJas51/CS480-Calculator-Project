85f39c5ee964026c927d45bbfe051b2d23a094fd
Kolby Miller <82845489+ProJas51@users.noreply.github.com>
2021-11-29 18:44:43 -0800

    Add files via upload

---
+++ Source_Code_Miller_Kolby/CalcTest.js
@@ -0,0 +1,33 @@
+console.log("This program will generate automatic test cases for the calculator program created in lab 3."); 
+
+
+const isValid = true; 
+
+let testCase = []; 
+
+// this function will take in an array as the generated expression, and add either a number, operator, or parentheses to the expression 
+function fillExpression() { 
+
+    // for loop to update the expression array 
+    for (let i = 0; i < testCase.length; i++) { 
+        
+        // traverse calc_buttons 
+        for (let j = 0; j < calc_buttons.length; j++) { 
+            
+            // everytime a valid character is met, compare to the value in expression 
+            calc_buttons[j]; 
+        }
+
+        if (testCase[i] == calc_buttons[j]) { 
+
+        }
+
+    }
+    
+}
+
+function evalExpression() { 
+
+    console.log(eval(testCase)); 
+
+} 
